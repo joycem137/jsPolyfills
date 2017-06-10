4a7da020e6c6a504e0f7f58e5f37e3aebe0e9c1a
Joyce Mayorga <joycem137@gmail.com>
2017-06-09 22:44:56 -0700

    Added Math.hypot

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ if (Array.prototype.find === undefined) {
     }
 }
 
+// Math libraries
+Math.hypot = Math.hypot || function() {
+    var y = 0;
+    var length = arguments.length;
+
+    for (var i = 0; i < length; i++) {
+        if (arguments[i] === Infinity || arguments[i] === -Infinity) {
+            return Infinity;
+        }
+        y += arguments[i] * arguments[i];
+    }
+    return Math.sqrt(y);
+};
+
 // Object improvements!
 if (Object.values === undefined) {
     Object.values = function values(object) {
